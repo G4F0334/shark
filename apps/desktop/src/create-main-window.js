db042e225f9303e44a36251b5ec2039f74cf18d7
G4F0334 <vlade42n42@gmail.com>
2026-07-03 00:06:50 +0500

    new vers and host

--- apps/desktop/src/create-main-window.js
+++ apps/desktop/src/create-main-window.js
@@ -10,7 +10,7 @@ function getTargetUrl() {
     return 'http://localhost:5173';
   }
 
-  return 'https://gentleman-minimal-furniture-camcorder.trycloudflare.com/';
+  return 'https://minnesota-squad.ru/';
 
   // return 'https://sharkapi.ping-net.kz/';
 }
--- apps/server/src/utils/env.ts
+++ apps/server/src/utils/env.ts
@@ -12,7 +12,7 @@ const SERVER_VERSION =
 const DESKTOP_VERSION =
   typeof process.env.SHARKORD_DESKTOP_VERSION !== 'undefined'
     ? process.env.SHARKORD_DESKTOP_VERSION
-    : '0.0.25';
+    : '0.0.26';
 
 const DESKTOP_DOWNLOAD_URL = process.env.SHARKORD_DESKTOP_DOWNLOAD_URL || 'http://ping-net.ddns.net:4896/win-unpacked.7z';
 
